65640ffefc1b8f5459c28a8394df28b3f1c5f893
Acuzito55 <asbsola@gmail.com>
2024-05-16 16:27:25 -0300

    fix: syscall.h conditional inclusion

--- Kernel/include/syscall.h
+++ Kernel/include/syscall.h
@@ -1,6 +1,6 @@
-#include <stdint.h>
 #ifndef _SYSCALL_H_
 #define _SYSCALL_H_
+#include <stdint.h>
 
 uint64_t syscall_handler(uint64_t rax, uint64_t rdi, uint64_t rsi, uint64_t rdx, uint64_t r10, uint64_t r8, uint64_t r9);
 
--- Kernel/include/videoDriver.h
+++ Kernel/include/videoDriver.h
@@ -1,3 +1,7 @@
+#ifndef _VIDEO_H_
+#define _VIDEO_H_
 #include <stdint.h>
 
 void putPixel(uint32_t hexColor, uint64_t x, uint64_t y);
+
+#endif

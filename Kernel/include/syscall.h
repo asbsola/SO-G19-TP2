d3dfe2871c43c87e5512fa21213fe1b76b4893e3
RondaAgustin <agusleonel.r@gmail.com>
2024-05-19 14:02:20 -0300

    change include in syscall.h

--- Kernel/include/syscall.h
+++ Kernel/include/syscall.h
@@ -1,6 +1,6 @@
 #ifndef _SYSCALL_H_
 #define _SYSCALL_H_
-#include <stdint.h>
+#include <int_types.h>
 
 uint64_t sys_read(uint64_t rdi, uint64_t rsi, uint64_t rdx, uint64_t r10, uint64_t r8, uint64_t r9);
 uint64_t sys_write(uint64_t rdi, uint64_t rsi, uint64_t rdx, uint64_t r10, uint64_t r8, uint64_t r9);

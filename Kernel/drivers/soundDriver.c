e0236f5a6638510f31db66cc6810267c19846aa8
Acuzito55 <asbsola@gmail.com>
2024-05-22 16:36:32 -0300

    fix: division by 0 on beep checked

--- Kernel/drivers/soundDriver.c
+++ Kernel/drivers/soundDriver.c
@@ -4,6 +4,7 @@
 #include <lib.h>
 
 void beep(uint64_t freq, uint64_t milis) {
+    if(freq == 0) return;
     play_sound(freq);
     int start = ticks_elapsed();
     while(ticks_elapsed() - start < TICKS_PER_SECOND * milis / 1000);

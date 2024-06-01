00da124c447764559bf2da0c16295fce902c5312
IgnacioSearles <ignacio.searles@gmail.com>
2024-06-01 16:36:39 -0300

    feat: added max screen refresh rate

--- Kernel/include/drivers/pitDriver.h
+++ Kernel/include/drivers/pitDriver.h
@@ -3,6 +3,9 @@
 
 #include <registers.h>
 
+#define UPDATE_SCREEN_FREQ 30 
+#define MAX_FREQUENCY 1193182
+
 void initialize_pit(uint32_t frequency);
 void timer_handler(const registers64_t*);
 void delay(uint64_t milis);

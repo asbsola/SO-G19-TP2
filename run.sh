7934c542422000f4aee9da33838382e73c612b92
IgnacioSearles <ignacio.searles@gmail.com>
2024-10-09 18:49:33 -0300

    config: gdb

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ "$1" = "-d" ]; then
-qemu-system-x86_64 -s -S -hda Image/x64BareBonesImage.qcow2 -m 512 #-soundhw pcspk
+qemu-system-x86_64 -s -d int -S -hda Image/x64BareBonesImage.qcow2 -m 512 #-soundhw pcspk
 else
 qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 #-soundhw pcspk
 fi

2b0dba88d6199961581e42d9c987d5ba7b430647
IgnacioSearles <ignacio.searles@gmail.com>
2024-10-10 13:26:44 -0300

    fix: fixed makefile for gdb support (thanks aGODio)

--- Kernel/idle.c
+++ Kernel/idle.c
@@ -17,6 +17,6 @@ uint64_t idle(char** argv, int argc){
     write_to_video_text_buffer("GRUPO 21\n", 9, 0x006fb5fb);
 
     create_process(the_process_manager, 0, NOT_IN_FOREGROUND, (uint64_t (*)(char**, int))(SHELL_CODE_ADDRESS), argv);
-    
+
     while(1) _hlt();
 }

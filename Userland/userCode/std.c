e48edcca3c7593bd4f8c5c346741b56496f63fa0
santiagoBassi <santiagobassi21@gmail.com>
2024-10-14 13:52:32 -0300

    pvs

--- Userland/userCode/std.c
+++ Userland/userCode/std.c
@@ -1,3 +1,5 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 #include <std.h>
 
 uint32_t strcmp(const char* s1, const char* s2) {

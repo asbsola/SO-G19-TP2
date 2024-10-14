cab19933d327d00785bb963fe6d0e2ce67d75e8f
santiagoBassi <santiagobassi21@gmail.com>
2024-10-14 15:54:13 -0300

    Loop command

--- Userland/userCode/include/shell.h
+++ Userland/userCode/include/shell.h
@@ -44,6 +44,7 @@ uint64_t kill(char** argv, int argc);
 uint64_t cleanup(char** argv, int argc);
 uint64_t block(char** argv, int argc);
 uint64_t unblock(char** argv, int argc);
+uint64_t loop(char** argv, int argc);
 
 void run_shell();
 char** get_args(const char* input, int* argc);

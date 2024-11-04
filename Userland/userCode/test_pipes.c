044b460b6cfff3109bbaa2a7c85e3b85f9c7bd76
Augusto Barthelemy <asbsola@gmail.com>
2024-11-04 17:40:27 -0300

    feat: test_pipes improved

--- Userland/userCode/test_pipes.c
+++ Userland/userCode/test_pipes.c
@@ -2,72 +2,103 @@
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 #include <std.h>
 #include <stddef.h>
+#include <test_utils.h>
 
-#define TOTAL_PROCESSES 250
+#define TOTAL_PAIR_PROCESSES (TOTAL_PROCESSES/2)
+#define MAX_LEN 250
 #define PIPE_NAME "test_pipe"
 
 uint64_t writer(char **argv, int argc) {
-    int n = atoi(argv[1]);
-    char buff[] = {n};
-    sys_write(sys_get_stdout(), buff, 1);
+    char buff[MAX_LEN];
+    int len = atoi(argv[2]);
+    for(int i=0; i<len; i++){
+        buff[i] = argv[1][i] - '0';
+    }
+    sys_write(sys_get_stdout(), buff, len);
     return 0;
 }
 
 uint64_t reader(char **argv, int argc) {
-    char buff[1];
-    sys_read(sys_get_stdin(), buff, 1);
-    return buff[0];
+    char buff[MAX_LEN];
+    int len = atoi((argv[1]));
+    sys_read(sys_get_stdin(), buff, len);
+    int res = 0;
+    for(int i=0; i<len; i++) res += buff[i];
+    return res;
 }
 
 uint64_t test_pipes(char **argv, int argc) {
-    pid_t pids[TOTAL_PROCESSES];
-    int max_processes;
-    char buff[3];
-    char* writerArgv[] = {"writer", buff, NULL};
-    char* readerArgv[] = {"reader", NULL};
+    pid_t r_pids[TOTAL_PAIR_PROCESSES];
+    int max_pair_processes;
+    int msg_len;
+    char buff[MAX_LEN] = {0};
+    char len_w[3] = {0};
+    char len_r[3] = {0};
+    char* writerArgv[] = {"writer", buff, len_w, NULL};
+    char* readerArgv[] = {"reader", len_r, NULL};
     int sent = 0;
     int64_t res = 0;
 
-    if (argc < 2) {
-        puts_with_color("test_pipes: ERROR must provide max_processes (tops at 250)\n", 0xFF0000);
+    if (argc < 3) {
+        puts_with_color("test_pipes: ERROR must provide msg_len and max_pair_processes (tops at 125)\n", 0xFF0000);
         return -1;
     }
 
-    max_processes = atoi(argv[1]);
-    if (max_processes <= 0) {
-        puts_with_color("test_wait: ERROR max_processes must be greater than 0\n", 0xFF0000);
+    msg_len = atoi(argv[2]);
+    if (msg_len <= 0) {
+        puts_with_color("test_pipes: ERROR msg_len must be greater than 0\n", 0xFF0000);
         return -1;
     }
-    if(max_processes > TOTAL_PROCESSES){
-        puts_with_color("test_wait: ERROR max_processes must be less than 250\n", 0xFF0000);
+    if(msg_len > MAX_LEN){
+        puts_with_color("test_pipes: ERROR msg_len must be less than 250\n", 0xFF0000);
+        return -1;
+    }
+
+    max_pair_processes = atoi(argv[2]);
+    if (max_pair_processes <= 0) {
+        puts_with_color("test_pipes: ERROR max_pair_processes must be greater than 0\n", 0xFF0000);
+        return -1;
+    }
+    if(max_pair_processes > TOTAL_PAIR_PROCESSES){
+        puts_with_color("test_pipes: ERROR max_pair_processes must be less than 250\n", 0xFF0000);
         return -1;
     }
 
     fd_t pipe = sys_pipe_open(PIPE_NAME);
 
-    for(int i=1; i<=max_processes; i++){
-        itoa(i, writerArgv[1], 3);
-        sent += i;
-        sys_create_process(writer, writerArgv, sys_get_stdin(), pipe);
+    for(int i=0; i<max_pair_processes; i++){
+        for(int j=0; j<msg_len; j++){
+            int val = GetUniform(10);
+            sent += val;
+            writerArgv[1][j] = val + '0';
+        }
+        itoa(msg_len, writerArgv[2], 3);
+        itoa(msg_len, readerArgv[1], 3);
+        pid_t w_pid = sys_create_process(writer, writerArgv, sys_get_stdin(), pipe);
+        r_pids[i] = sys_create_process(reader, readerArgv, pipe, sys_get_stdout());
+        if(w_pid == -1 || r_pids[i] == -1){
+            puts_with_color("test_pipe: ERROR creating process\n", 0xFF0000);
+            sys_pipe_close_named(PIPE_NAME);
+            return -1;
+        }
     }
 
-    for(int i=0; i<max_processes; i++)
-        pids[i] = sys_create_process(reader, readerArgv, pipe, sys_get_stdout());
-
-    for(int i=0; i<max_processes; i++){
+    for(int i=0; i<max_pair_processes; i++){
         int64_t ret;
-        sys_wait_pid(pids[i], &ret);
+        sys_wait_pid(r_pids[i], &ret);
         res += ret;
     }
 
     sys_pipe_close_named(PIPE_NAME);
 
-    if(sent < res){
+    if(sent > res){
         puts_with_color("Missing sent values\n", 0x00ff0000);
+        printf("Expected %d but received %d\n", sent, res);
         return -1;
     }
-    if(sent > res){
+    if(sent < res){
         puts_with_color("Received extranious values\n", 0x00ff0000);
+        printf("Expected %d but received %d\n", sent, res);
         return -1;
     }
     
